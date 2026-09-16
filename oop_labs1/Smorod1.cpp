037e8aafb9fa36846cc4e2cea33f9cf3399bca52
paV <tsaritsonpavel@gmail.com>
2026-09-16 23:41:25 +0500

    Add Doxygen doc

--- oop_labs1/Smorod1.cpp
+++ oop_labs1/Smorod1.cpp
@@ -1,22 +1,32 @@
  #include <iostream>
  using namespace std;
  
+ /// @brief Заполняет статический массив числами, введенными с клавиатуры
+ /// @param arr Ссылка на массив из 10 целых чисел, который нужно заполнить
  void fillArray(int (&arr)[10]){
     for (int i=0; i<10; i++){
         cin >> arr[i];
     }
  }
+ /// @brief Выводит все элементы массива через пробел
+ /// @param arr Константная ссылка на массив (только для чтения)
  void printArray(const int(&arr)[10]){
     for (auto x: arr){
         cout << x<< " ";
     }
     cout << "\n";
  }
+ /// @brief Меняет местами два элемента массива по индексам
+ /// @param arr Ссылка на массив (будем менять)
+ /// @param idx1 Ссылка на индекс первого элемента
+ /// @param idx2 Ссылка на индекс второго элемента
  void swapElements(int (&arr)[10], int &idx1, int &idx2){
     int musor = arr[idx1];
     arr[idx1] = arr[idx2];
     arr[idx2] = musor;
  }
+ /// @brief Умножает каждый элемент массива на 2
+ /// @param arr Ссылка на массив (будем менять)
  void multiplyByTwo(int (&arr)[10]){
     for (int& x :arr){
         x*=2;
