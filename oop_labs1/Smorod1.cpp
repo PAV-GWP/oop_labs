5c88e2cff2ccfd82982bf67e08537821566856b7
paV <tsaritsonpavel@gmail.com>
2026-09-16 22:21:54 +0500

    add printArray

--- oop_labs1/Smorod1.cpp
+++ oop_labs1/Smorod1.cpp
@@ -1,13 +1,21 @@
  #include <iostream>
-
+ using namespace std;
+ 
  void fillArray(int (&arr)[10]){
     for (int i=0; i<10; i++){
-        std::cin >> arr[i];
+        cin >> arr[i];
     }
+ }
+ void printArray(const int(&arr)[10]){
+    for (auto x: arr){
+        cout << x<< " ";
+    }
+    cout << "\n";
  }
 
  int main(){
     int arr[10];
     fillArray(arr);
+    printArray(arr);
     return 0;
  }
