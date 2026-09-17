3f49b9a307413af96a54cb1ca855bc33493b09e3
paV <tsaritsonpavel@gmail.com>
2026-09-17 07:07:15 +0500

    add int& getElement and new main (getElement(myArr, 2) = 999;)

--- oop_labs1/Smorod1.3.cpp
+++ oop_labs1/Smorod1.3.cpp
@@ -12,9 +12,20 @@
     arr.data = new int[size]{};
     return arr;
  }
-
+ int& getElement(safeArray& arr, int index){
+    static int dummy = -999;
+    if (index <0 || index>=arr.size){
+        cout << "Error";
+        return dummy;
+    }
+    return arr.data[index];
+ }
  int main (){
     int N;
     cout << "N: "; cin >> N;
-    
+    safeArray myArr = createArray(N);
+    getElement(myArr, 2) = 999; // 999 на втором индексе
+    cout << getElement(myArr,2);
+    delete[] myArr.data;
+    myArr.data = nullptr;
  }
