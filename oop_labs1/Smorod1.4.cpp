2fca51cf57b9f67ea70de945cc63804147577ea2
paV <tsaritsonpavel@gmail.com>
2026-09-17 08:36:10 +0500

    add printmatrix

--- oop_labs1/Smorod1.4.cpp
+++ oop_labs1/Smorod1.4.cpp
@@ -15,3 +15,21 @@
         }
     }
  }
+ void printMatrix(int** matrix, int rows, int cols, bool showBorders = true, std::string title = "Matrix"){
+    cout << "=== " << title << " ===\n";
+    int width = cols * 6;
+    if (showBorders){
+        for (int w=0; w< width; w++){
+            cout << "-";
+        }
+        cout << "\n";
+    }
+    for (int i=0; i<rows;i++){
+        if (showBorders) cout << "| ";
+        for (int j = 0; j < cols; j++) {
+            cout << matrix[i][j] << "\t";
+        }
+        if (showBorders) cout << "|";
+        cout << "\n";
+    }
+ }
