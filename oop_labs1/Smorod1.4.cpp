d2f4eab1c210eb15abf78cc6aed76fd202da5988
paV <tsaritsonpavel@gmail.com>
2026-09-17 14:08:18 +0500

    add doxygen doc

--- oop_labs1/Smorod1.4.cpp
+++ oop_labs1/Smorod1.4.cpp
@@ -1,6 +1,9 @@
  #include <iostream>
  using namespace std;
-
+ /// @brief Выделяет динамическую память под двумерный массив (матрицу)
+ /// @param rows Количество строк (студентов)
+ /// @param cols Количество столбцов (предметов)
+ /// @return Указатель на выделенный двумерный массив (int**)
  int** allocateMatrix(int rows, int cols){
     int** matrix = new int*[rows];
     for (int i=0; i<rows; i++){
@@ -8,6 +11,10 @@
     }
     return matrix;
  }
+ /// @brief Заполняет матрицу значениями (оценками студентов) с клавиатуры
+ /// @param matrix Указатель на матрицу
+ /// @param rows Количество строк
+ /// @param cols Количество строк
  void fillMatrix(int** matrix, int rows, int cols){
     cout << "Enter grades for " << rows << " students and " << cols << " subjects:\n";
     for (int i = 0; i < rows; i++){
@@ -19,6 +26,12 @@
     }
     cout << "\n";
  }
+ /// @brief Выводит матрицу на экран с заголовком и опциональной рамкой
+ /// @param matrix Указатель на матрицу
+ /// @param rows Количество строк
+ /// @param cols Количество столбцов
+ /// @param showBorders Флаг отображения рамки (по умолчанию true)
+ /// @param title Заголовок матрицы (по умолчанию "Matrix")
  void printMatrix(int** matrix, int rows, int cols, bool showBorders = true, std::string title = "Matrix"){
     cout << "===" << title << " ===\n";
     int width = cols * 4;
@@ -37,13 +50,17 @@
         cout << "\n";
     }
  }
+ /// @brief Корректно очищает динамическую память двумерного массива
+ /// @param matrix Указатель на матрицу
+ /// @param rows Количество строк
  void freeMatrix(int**matrix, int rows){
     for ( int i =0; i<rows; i++){
         delete[] matrix[i];
     }
     delete[]matrix;
  }
-
+ /// @brief Главная функция программы: тестирование выделения памяти, ввода, вывода и очистки
+ /// @return
  int main(){
     int rows =3; int cols = 3;
     int** myMatrix= allocateMatrix(rows, cols);
